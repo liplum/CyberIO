ea0ad16e0f3dc7bd440fbadf6e1df921b63cb4b0
tudouman <tudou212665833@outlook.com>
2022-02-21 20:26:03 +0800

    Update Prism.kt
    [NR]普通炮台v0.0001

--- src/net/liplum/blocks/prism/Prism.kt
+++ src/net/liplum/blocks/prism/Prism.kt
@@ -1,21 +1,29 @@
 package net.liplum.blocks.prism
 
+import mindustry.content.Items
 import mindustry.entities.bullet.LaserBulletType
 import mindustry.gen.Building
 import mindustry.gen.Bullet
 import mindustry.gen.Groups
 import mindustry.gen.Unit
+import mindustry.type.Category
+import mindustry.type.Item
 import mindustry.world.Block
+import mindustry.world.blocks.defense.turrets.Turret
 
-open class Prism(name: String) : Block(name) {
+open class Prism(name: String) : Turret(name) {
+
+    //open class Prism(name: String) : Block(name) {
     //copy your lasers
     //
     var range = 30f
     fun copylaser() {}
     fun callAttack(ownunit: Unit) {}
     fun rebound(laserbullet: LaserBulletType) {}
+    fun fire(ownunit: Unit){}
 
     init {
+
         solid = true
         update = true
         absorbLasers = true
@@ -31,12 +39,18 @@ open class Prism(name: String) : Block(name) {
             ) {
                 it.absorb()
             }
+        /*Groups.bullet.each({
+            it.type.absorbable
+        }) {
+            it.absorb()
+        }*/
 
-            /*Groups.bullet.each({
-                it.type.absorbable
-            }) {
-                it.absorb()
-            }*/
+        }
+        
+    }
+    open fun alertstate(){
+        if(true){
+
         }
     }
 }
