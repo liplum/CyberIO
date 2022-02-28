415b258b7e5e393cf6599f499fba095ec1717279
liplum <Li_plum@outlook.com>
2022-02-28 12:54:40 +0800

    Prism

--- src/net/liplum/blocks/prism/Prism.kt
+++ src/net/liplum/blocks/prism/Prism.kt
@@ -1,19 +1,27 @@
 package net.liplum.blocks.prism
 
-import arc.Core
 import arc.graphics.g2d.Draw
+import mindustry.entities.bullet.BasicBulletType
 import mindustry.entities.bullet.LaserBulletType
 import mindustry.gen.Groups
 import mindustry.graphics.Drawf
 import mindustry.graphics.Layer
 import mindustry.world.blocks.defense.turrets.PowerTurret
 import net.liplum.math.PolarPos
+import net.liplum.utils.Util2D
+import net.liplum.utils.copy
+
+enum class PrismData {
+    Duplicate
+}
 
 open class Prism(name: String) : PowerTurret(name) {
     //open class Prism(name: String) : Block(name) {
     //copy your lasers
     //
     var realrange = 30f
+    var deflection = 15f
+    var prismRange = 10f
 
     init {
         solid = true
@@ -27,16 +35,34 @@ open class Prism(name: String) : PowerTurret(name) {
     }
 
     open inner class PrismBuild : PowerTurretBuild() {
-        var selfPos: PolarPos = PolarPos(10f,0f)
+        var selfPos: PolarPos = PolarPos(20f, 0f)
+        open val prismX: Float
+            get() = x + selfPos.toX()
+        open val prismY: Float
+            get() = y + selfPos.toY()
+
         override fun updateTile() {
+            selfPos.a += 0.05f * delta()
+            //selfPos.r += 0.05f
             Groups.bullet.intersect(
                 x - 10f,
                 y - 10f,
                 realrange,
                 realrange
             ) {
-                if (it.type is LaserBulletType) {
+                val btype = it.type
+                if (btype is LaserBulletType) {
                     it.absorb()
+                } else if (btype is BasicBulletType) {
+                    if (Util2D.distance(it.x, it.y, prismX, y + prismY) < prismRange) {
+                        if (it.data != PrismData.Duplicate) {
+                            val angle = it.rotation()
+                            it.data = PrismData.Duplicate
+                            val copy = it.copy()
+                            it.rotation(angle - deflection)
+                            copy.rotation(angle + deflection)
+                        }
+                    }
                 }
             }
         }
@@ -47,18 +73,19 @@ open class Prism(name: String) : PowerTurret(name) {
 
             Draw.z(Layer.turret)
             tr2.trns(rotation, -recoil)
-            selfPos.a += 0.1f
-            selfPos.r += 0.05f
+            val prismX = prismX
+            val prismY = prismY
             Drawf.shadow(
                 region,
-                selfPos.toX() + x + tr2.x - elevation,
-                selfPos.toY() + x + tr2.y - elevation
+                prismX + tr2.x - elevation,
+                prismY + tr2.y - elevation
             )
             Draw.rect(
                 region,
-                selfPos.toX() + x + tr2.x,
-                selfPos.toY() + y + tr2.y
+                prismX + tr2.x,
+                prismY + tr2.y
             )
+            Drawf.circles(prismX, prismY, prismRange)
         }
     }
 }
