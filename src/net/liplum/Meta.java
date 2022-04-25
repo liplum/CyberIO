f534a2d1b9bb18f8f9281be68ffea0804cb6439a
liplum <Li_plum@outlook.com>
2022-04-25 19:33:20 +0800

    [AR] update to v3.1

--- src/net/liplum/Meta.java
+++ src/net/liplum/Meta.java
@@ -8,7 +8,7 @@ public class Meta {
     public static final String Name = "Cyber IO";
     public static final String Version = "v3";
     public static final String DefaultLang = "en";
-    public static final Version2 DetailedVersion = new Version2(2, 1);
+    public static final Version2 DetailedVersion = new Version2(3, 1);
     public static final String Repo = "liplum/CyberIO";
     public static final String Discord = "https://discord.gg/PDwyxM3waw";
     public static final String LatestRelease = "https://api.github.com/repos/" + Repo + "/releases/latest";
