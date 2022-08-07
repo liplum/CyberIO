6154103b0f8ca8c1db175906c3b7244b45ae53b6
liplum <Li_plum@outlook.com>
2022-08-08 05:11:46 +0800

    Improved CutteX

--- main/src/net/liplum/bullet/ArcFieldBulletType.kt
+++ main/src/net/liplum/bullet/ArcFieldBulletType.kt
@@ -5,22 +5,34 @@ import arc.graphics.g2d.Lines
 import arc.math.Angles
 import arc.math.Interp
 import arc.util.Time
+import arc.util.Tmp
 import mindustry.entities.Units
 import mindustry.entities.bullet.ContinuousBulletType
 import mindustry.gen.Bullet
+import mindustry.world.blocks.ControlBlock
 import net.liplum.mdt.utils.MdtUnit
+import plumy.core.arc.lighten
 import plumy.core.math.Degree
 
 class ArcFieldBulletType : ContinuousBulletType() {
     @JvmField var angle: Degree = 80f
     @JvmField var lengthInterp: Interp = Interp.slope
-    @JvmField var fieldAlpha = 0.5f
+    @JvmField var fieldAlpha = 0.3f
     @JvmField var highlightTime = damageInterval * 5f
+    @JvmField var pointField = true
+    @JvmField var pointFieldEnabledWhenPlayer = true
+    @JvmField var lightenIntensity = -1f
 
     init {
         optimalLifeFract = 0.5f
         removeAfterPierce = false
         pierceArmor = true
+    }
+
+    override fun init() {
+        super.init()
+        if (!pointField)
+            pointFieldEnabledWhenPlayer = false
     }
 
     override fun init(b: Bullet) {
@@ -36,12 +48,11 @@ class ArcFieldBulletType : ContinuousBulletType() {
     override fun draw(b: Bullet) = b.run {
         val curLen = currentLength(this)
         Lines.stroke(curLen)
-        Draw.color(hitColor)
-        if (fdata < highlightTime) {
-            Draw.alpha(1f) // breath
-        } else {
-            Draw.alpha(0.5f) // breath
-        }
+        if (lightenIntensity > 0f)
+            Draw.color(Tmp.c1.set(hitColor).lighten(lightenIntensity))
+        else Draw.color(hitColor)
+        if (fdata < highlightTime) Draw.alpha(1f)
+        else Draw.alpha(fieldAlpha)
         Lines.arc(x, y, curLen / 2f, angle / 360f, this.rotation() - angle / 2f)
         Draw.reset()
     }
@@ -74,10 +85,28 @@ class ArcFieldBulletType : ContinuousBulletType() {
         this.collision(unit, unit.x, unit.y)
     }
 
-    override fun currentLength(b: Bullet): Float {
-        return length * b.fin(lengthInterp)
+    fun isControlledByPlayer(b: Bullet): Boolean {
+        val turret = b.owner as? ControlBlock
+        return turret?.isControlled ?: false
+    }
+
+    override fun currentLength(b: Bullet): Float = b.run {
+        if (pointField) {
+            if (pointFieldEnabledWhenPlayer && isControlledByPlayer(this)) {
+                return@run Tmp.v1.set(aimX, aimY).sub(x, y).len()
+            } else {
+                return@run length * b.fin(lengthInterp)
+            }
+        } else {
+            return@run length * b.fin(lengthInterp)
+        }
     }
 
     override fun drawLight(b: Bullet) {
+    }
+
+    companion object {
+        operator fun invoke(config: ArcFieldBulletType.() -> Unit) =
+            ArcFieldBulletType().apply(config)
     }
 }
--- main/src/net/liplum/registry/CioSpaceship.kt
+++ main/src/net/liplum/registry/CioSpaceship.kt
@@ -7,6 +7,7 @@ import mindustry.world.blocks.defense.turrets.ContinuousLiquidTurret
 import mindustry.world.meta.BuildVisibility
 import net.liplum.DebugOnly
 import net.liplum.R
+import net.liplum.S
 import net.liplum.annotations.DependOn
 import net.liplum.bullet.ArcFieldBulletType
 import net.liplum.mdt.utils.addAmmo
@@ -19,16 +20,34 @@ object CioSpaceship {
     )
     fun cuttex() {
         DebugOnly {
-            cuttex = ContinuousLiquidTurret("cuttex").apply {
+            cuttex = ContinuousLiquidTurret("cuttex").apply turret@{
                 category = Category.turret
                 buildVisibility = BuildVisibility.shown
                 size = 3
                 shootSound = Sounds.none
                 shootY = 2.8f
-                addAmmo(Liquids.water, ArcFieldBulletType().apply {
+                rotateSpeed = 8f
+                range = 160f
+                addAmmo(Liquids.water, ArcFieldBulletType {
                     hitColor = R.C.CuttexCyan
-                    damage = 20f
-                    length = 150f
+                    damage = 12f
+                    length = 100f
+                })
+                addAmmo(Liquids.cryofluid, ArcFieldBulletType {
+                    hitColor = R.C.CuttexCyan
+                    damage = 40f
+                    length = 130f
+                })
+                addAmmo(CioFluid.cyberion, ArcFieldBulletType {
+                    hitColor = S.Hologram
+                    damage = 140f
+                    length = 145f
+                    lightenIntensity = 0.2f
+                })
+                addAmmo(Liquids.cyanogen, ArcFieldBulletType {
+                    hitColor = R.C.CuttexCyan
+                    damage = 180f
+                    length = 160f
                 })
             }
         }
